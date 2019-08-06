45f86f452e9e099f1967e1c6b3b97b646dd41d14
scaperex <omer.shubi@gmail.com>
2019-08-06 23:24:54 +0300

    Start work on KNN

--- main.py
+++ main.py
@@ -6,13 +6,9 @@ import Model
 
 def main():
 
-    model = Model('.')
+    model = Model.Model('.')
     predictions = model.predict("reuters_test_data")
-
-    rawdata = parsing.parsing_data("Raw Data - DO NOT CHANGE/reuters_train_data")
-    print("finished parsing")
-    data = File_reader.File_reader(rawdata)
-    train_features_raw, train_labels_raw = data.build_set_tfidf()
+    print(predictions)
 
 
 if __name__ == "__main__":
