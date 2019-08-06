2ec5a175ce3f9b061340dc3272fd184fed9e826f
Nitzan Shamir <shamirnitzan@campus.htecnion.ac.il>
2019-08-06 10:58:47 +0300

    cacluations class

---
+++ Calculations.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class Calulations:
+
+    @staticmethod
+    def cosine_similarity(list1, list2):
+        """
+        Calculates cosine similarity between two lists
+
+        Assumes lists are of same length
+        :param list1: list
+        :param list2: list
+        :return: cosine similarity
+        """
+        return 1-(np.dot(list1, list2) / (np.linalg.norm(list1) * np.linalg.norm(list2)))
+
+    @staticmethod
+    def distances(test, train):
+        """
+        Calculates distances between test and train
+
+        :param test:
+        :param train:
+        :return: distances list
+        """
+        distances = []
+        for i in range(len(test)):
+            distances.append(Calulations.cosine_similarity(test[i], train))
+        return distances
+
+
+
