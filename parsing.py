0f8450bf8e1f02ebebc72da7d20f66c603ecf9b6
ploznik <ploznik@campus.technion.ac.il>
2019-08-07 17:58:11 +0300

    add try and except in parsing

--- parsing.py
+++ parsing.py
@@ -86,7 +86,7 @@ def parsing_data(directory_path, is_test):
     for root, dirs, files in os.walk(directory_path, topdown=False):
         for name in files:
             try:
-                data = list(filter(lambda x: x['labels'] != [] and len(x['text']) < 10 ,parsing(os.path.join(root, name), is_test)))
+                data = list(filter(lambda x: x['labels'] != [] and x['text'] != '' ,parsing(os.path.join(root, name), is_test)))
                 print("number of articles in file ",name, len(data))
                 final_data = final_data + data
             except UnicodeDecodeError:
