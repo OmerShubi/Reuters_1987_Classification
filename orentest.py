191609c28f472ee10ef174f0855a8cb124619247
ploznik <ploznik@campus.technion.ac.il>
2019-08-07 14:25:52 +0300

    add try and except in parsing

--- orentest.py
+++ orentest.py
@@ -1,19 +1,25 @@
 def create_cities_dict(labels_pool):
-    city_country_list = []
-    with open("cities.csv") as f:
-        for line in f:
-            city_country_list.append(line.split())
+    """
 
+    :param labels_pool:
+    :return: cities to country dictionary
+    """
+    city_country_list = []
+    with open("world-cities_csv.csv", encoding="iso-8859-1") as f:
+        for line in f:
+            city_country_list.append(list(map(lambda x: x.replace('\n', ""), line.split(','))))
 
     current_labels = []
     for city_country in city_country_list:
         if city_country[1] in labels_pool:
-            clean_list = list(map(lambda x: x.replace(',', ""), city_country))
+            clean_list = list(map(lambda x: x.replace('\n', ""), city_country))
             current_labels.append(clean_list)
 
     cities_dict = {elem[0]:elem[1] for elem in current_labels}
 
     return cities_dict
+
+
 print(create_cities_dict(["china","usa"]))
 # TODO check about r0009.sgm
 # TODO Dateline
